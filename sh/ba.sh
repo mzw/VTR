5bbf090a03a31c39ad079dbd3a8cce0281951ee4
Yuta Maezawa <maezawa@nii.ac.jp>
2016-09-08 19:55:19 +0900

    invoke CLI.cov at ba.sh

--- sh/ba.sh
+++ sh/ba.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sh/run dict vtr-example subjects/vtr-example refs/heads/master
+# sh/run dict vtr-example subjects/vtr-example refs/heads/master
+sh/run cov vtr-example subjects/vtr-example
