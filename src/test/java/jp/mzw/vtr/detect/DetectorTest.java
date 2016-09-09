26cac2fc899c39bd634f0df7a599643d568481f0
Yuta Maezawa <maezawa@nii.ac.jp>
2016-09-09 17:22:09 +0900

    modify test method name for the detection

--- src/test/java/jp/mzw/vtr/detect/DetectorTest.java
+++ src/test/java/jp/mzw/vtr/detect/DetectorTest.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.text.ParseException;
 
 import jp.mzw.vtr.core.Config;
-//import jp.mzw.vtr.dict.DictionaryBase;
 import jp.mzw.vtr.git.CheckoutConductor;
-//import jp.mzw.vtr.git.Commit;
 import jp.mzw.vtr.git.GitUtils;
 
 import org.eclipse.jgit.api.Git;
@@ -31,18 +29,8 @@ public class DetectorTest {
 		this.config = new Config("config.properties");
 	}
 
-//	@Test
-//	public void testOnCheckout() {
-//		try {
-//			Detector detector = new Detector(SUBJECT_ID, PATH_TO_SUBJECT, this.config);
-//			detector.onCheckout(new Commit("fcf9382884874b7ceecc16cd2155ab73b1346931", DictionaryBase.SDF.parse("2016-03-05 15:47:44 +0900")));
-//		} catch (Exception e) {
-//			Assert.fail();
-//		}
-//	}
-
 	@Test
-	public void testRevertGitRepository() throws IOException, ParseException, GitAPIException {
+	public void testDetectingSubjectTestCaseModification() throws IOException, ParseException, GitAPIException {
 		try {
 			CheckoutConductor cc = new CheckoutConductor(this.git, new File(PATH_TO_OUTPUT_DIR));
 			cc.addListener(new Detector(SUBJECT_ID, PATH_TO_SUBJECT, this.config));
@@ -51,5 +39,5 @@ public class DetectorTest {
 			Assert.fail();
 		}
 	}
-	
+
 }
