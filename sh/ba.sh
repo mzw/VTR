7b9f42b593c5d7c16c20b8e035363487824edab4
Yuta Maezawa <maezawa@nii.ac.jp>
2016-09-09 17:19:07 +0900

    imeplement CLI for the detection

--- sh/ba.sh
+++ sh/ba.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # sh/run dict vtr-example subjects/vtr-example refs/heads/master
-sh/run cov vtr-example subjects/vtr-example
+# sh/run cov vtr-example subjects/vtr-example
+sh/run detect vtr-example subjects/vtr-example
--- src/main/java/jp/mzw/vtr/CLI.java
+++ src/main/java/jp/mzw/vtr/CLI.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import jp.mzw.vtr.core.Config;
 import jp.mzw.vtr.cov.TestRunner;
+import jp.mzw.vtr.detect.Detector;
 import jp.mzw.vtr.dict.DictionaryMaker;
 import jp.mzw.vtr.git.CheckoutConductor;
 import jp.mzw.vtr.git.GitUtils;
@@ -29,8 +30,9 @@ public class CLI {
 		
 		if (args.length == 0) {
 			// Invalid usage
-			LOGGER.info("Ex) $ java jp.mzw.vtr.CLI dict vtr-example subjects/vtr-example refs/heads/master");
-			LOGGER.info("Ex) $ java jp.mzw.vtr.CLI cov vtr-example subjects/vtr-example");
+			LOGGER.info("Ex) $ java -cp=CLASSPATH jp.mzw.vtr.CLI dict vtr-example subjects/vtr-example refs/heads/master");
+			LOGGER.info("Ex) $ java -cp=CLASSPATH jp.mzw.vtr.CLI cov vtr-example subjects/vtr-example");
+			LOGGER.info("Ex) $ java -cp=CLASSPATH jp.mzw.vtr.CLI detect vtr-example subjects/vtr-example");
 		} else if ("dict".equals(args[0])) {
 			String subjectId = args[1];
 			String pathToSubject = args[2];
@@ -40,8 +42,11 @@ public class CLI {
 			String subjectId = args[1];
 			String pathToSubject = args[2];
 			cov(subjectId, pathToSubject, config);
+		} else if ("detect".equals(args[0])) {
+			String subjectId = args[1];
+			String pathToSubject = args[2];
+			detect(subjectId, pathToSubject, config);
 		}
-		
 	}
 	
 	private static void dict(File outputDir, String subjectId, String pathToSubject, String refToCompare) throws IOException, NoHeadException, GitAPIException {
@@ -56,8 +61,15 @@ public class CLI {
 
 	private static void cov(String subjectId, String pathToSubject, Config config) throws IOException, ParseException, GitAPIException {
 		Git git = GitUtils.getGit(pathToSubject);
-		CheckoutConductor cc = new CheckoutConductor(git, config.getOutputDir());
+		CheckoutConductor cc = new CheckoutConductor(git, new File(config.getOutputDir(), subjectId));
 		cc.addListener(new TestRunner(subjectId, new File(pathToSubject), config));
+		cc.checkout();
+	}
+	
+	private static void detect(String subjectId, String pathToSubject, Config config) throws IOException, ParseException, GitAPIException {
+		Git git = GitUtils.getGit(pathToSubject);
+		CheckoutConductor cc = new CheckoutConductor(git, new File(config.getOutputDir(), subjectId));
+		cc.addListener(new Detector(subjectId, pathToSubject, config));
 		cc.checkout();
 	}
 }
