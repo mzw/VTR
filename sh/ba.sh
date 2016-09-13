34dbcf97e3639b68deff294e9e1d9f8fdab70daf
Yuta Maezawa <maezawa@nii.ac.jp>
2016-09-13 13:37:03 +0900

    modify sh/ba.sh

--- sh/ba.sh
+++ sh/ba.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-#sh/run dict vtr-example subjects/vtr-example refs/heads/master
-sh/run cov vtr-example subjects/vtr-example
-#sh/run detect vtr-example subjects/vtr-example
+# sh/run dict vtr-example subjects/vtr-example refs/heads/master
+# sh/run cov vtr-example subjects/vtr-example
+# sh/run cov vtr-example subjects/vtr-example At fcf9382884874b7ceecc16cd2155ab73b1346931
+# sh/run cov vtr-example subjects/vtr-example After 7fcfdfa99bf9f220b9643f372c36609ca35c60b3
+# sh/run detect vtr-example subjects/vtr-example
 
+# sh/run dict commons-collections subjects/commons-collections refs/heads/trunk
+# sh/run cov commons-collections subjects/commons-collections
+# sh/run detect commons-collections subjects/commons-collections
 
+# sh/run dict commons-compress subjects/commons-compress refs/heads/master
+# sh/run cov commons-compress subjects/commons-compress
+# sh/run detect commons-compress subjects/commons-compress
