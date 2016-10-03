cca8c89fe9526f06a4b8c12af851563842927d16
Yuta Maezawa <maezawa@nii.ac.jp>
2016-10-03 12:44:34 +0900

    add test cases at ProjectTest

--- src/test/java/jp/mzw/vtr/VtrTestBase.java
+++ src/test/java/jp/mzw/vtr/VtrTestBase.java
@@ -10,6 +10,7 @@ public class VtrTestBase {
 
 	public static final String PROJECT_ID = "vtr-example";
 	public static final String PATH_TO_PROJECT_DIR = "src/test/resources/vtr-subjects/vtr-example";
+	public static final String TEST_CONFIG_FILENAME = "test-config.properties";
 	public static final String PATH_TO_OUTPUT_DIR = "src/test/resources/vtr-output/vtr-example";
 
 	protected Project project;
@@ -17,7 +18,7 @@ public class VtrTestBase {
 	@Before
 	public void _setup() throws IOException {
 		this.project = new Project(PROJECT_ID, PATH_TO_PROJECT_DIR);
-		this.project.setConfig("test-config.properties");
+		this.project.setConfig(TEST_CONFIG_FILENAME);
 	}
 	
 }
--- src/test/java/jp/mzw/vtr/core/ProjectTest.java
+++ src/test/java/jp/mzw/vtr/core/ProjectTest.java
@@ -2,6 +2,8 @@ package jp.mzw.vtr.core;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import jp.mzw.vtr.VtrTestBase;
@@ -9,11 +11,22 @@ import jp.mzw.vtr.VtrTestBase;
 public class ProjectTest extends VtrTestBase {
 
 	@Test
-	public void testConstructor() {
+	public void testConstructorWithNoConfig() {
 		Project project = new Project("vtr-example", "src/test/resources/vtr-example");
 		assertArrayEquals("vtr-example".toCharArray(), project.getProjectId().toCharArray());
 		assertArrayEquals("src/test/resources/vtr-example".toCharArray(), project.getProjectDir().getPath().toCharArray());
 		assertArrayEquals("output".toCharArray(), project.getOutputDir().getPath().toCharArray());
 		assertArrayEquals("/usr/local/apache-maven-3.3.9".toCharArray(), project.getMavenHome().getPath().toCharArray());
 	}
+
+	@Test
+	public void testConstructorWithConfig() throws IOException {
+		Project project = new Project("vtr-example", "src/test/resources/vtr-example");
+		project.setConfig(TEST_CONFIG_FILENAME);
+		assertArrayEquals("vtr-example".toCharArray(), project.getProjectId().toCharArray());
+		assertArrayEquals("src/test/resources/vtr-example".toCharArray(), project.getProjectDir().getPath().toCharArray());
+		assertArrayEquals("src/test/resources/vtr-output".toCharArray(), project.getOutputDir().getPath().toCharArray());
+		assertArrayEquals("/usr/local/apache-maven-3.3.9".toCharArray(), project.getMavenHome().getPath().toCharArray());
+	}
+
 }
