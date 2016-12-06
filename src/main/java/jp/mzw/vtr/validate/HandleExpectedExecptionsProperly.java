90a659282063bda51b4fdc101cffce8fcf0e418c
Yuta Maezawa <maezawa@nii.ac.jp>
2016-12-06 10:33:41 +0900

    implement HandleExpectedExceptionProperly validator without genPatch

--- src/main/java/jp/mzw/vtr/validate/HandleExpectedExecptionsProperly.java
+++ src/main/java/jp/mzw/vtr/validate/HandleExpectedExecptionsProperly.java
@@ -1,6 +1,8 @@
 package jp.mzw.vtr.validate;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jp.mzw.vtr.core.Project;
 import jp.mzw.vtr.git.Commit;
@@ -8,14 +10,21 @@ import jp.mzw.vtr.maven.MavenUtils;
 import jp.mzw.vtr.maven.TestCase;
 import jp.mzw.vtr.maven.TestSuite;
 
-import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HandleExpectedExecptionsProperly extends ValidatorBase {
+	protected static Logger LOGGER = LoggerFactory.getLogger(HandleExpectedExecptionsProperly.class);
 
 	public HandleExpectedExecptionsProperly(Project project) {
 		super(project);
 	}
-	
+
 	@Override
 	public void onCheckout(Commit commit) {
 		try {
@@ -24,23 +33,68 @@ public class HandleExpectedExecptionsProperly extends ValidatorBase {
 					if (this.dupulicates.contains(tc.getFullName())) {
 						continue;
 					}
-					if (!detect(commit, tc)) {
-						this.dupulicates.add(tc.getFullName());
-						MethodDeclaration method = tc.getMethodDeclaration();
-						ValidationResult vr = new ValidationResult(this.projectId, commit, tc, tc.getStartLineNumber(method), tc.getEndLineNumber(method), this);
-						this.validationResultList.add(vr);
-					}
+					detect(commit, tc);
 				}
 			}
 		} catch (IOException e) {
 			LOGGER.warn("Failed to checkout: {}", commit.getId());
 		}
 	}
-	
-	private boolean detect(Commit commit, TestCase tc) {
-		// try {...} catch(FooException e) { /* expected */ }
-		
-		return false;
+
+	private void detect(Commit commit, TestCase tc) throws IOException {
+		String content = FileUtils.readFileToString(tc.getTestFile());
+		// try {...Assert...} catch(FooException e) { /* expected */ }
+		for (ASTNode node : tc.getNodes()) {
+			if (node instanceof TryStatement) {
+				TryStatement tryStatement = (TryStatement) node;
+				if (this.hasAssertMethodInvocation(tryStatement.getBody())) {
+					catchExpectedException(commit, tc, content, (TryStatement) node);
+				}
+			}
+		}
+	}
+
+	private void catchExpectedException(Commit commit, TestCase tc, String content, TryStatement tryStatement) {
+		// ...} catch (FooException e) {...expect...}
+		for (Object object : tryStatement.catchClauses()) {
+			CatchClause cc = (CatchClause) object;
+			for (Comment comment : getComments(tc, cc)) {
+				String[] split = getContent(content, comment).split(" ");
+				for (String expect : split) {
+					if (expect.contains("expect")) {
+						// Prevent duplicated detection
+						this.dupulicates.add(tc.getFullName());
+						// Register detection result
+						ValidationResult vr = new ValidationResult(this.projectId, commit, tc,
+								tc.getStartLineNumber(cc), tc.getEndLineNumber(cc), this);
+						this.validationResultList.add(vr);
+						LOGGER.info("Detect {} in {} at {}", this.getClass().getName(), tc.getFullName(), commit.getId());
+					}
+				}
+			}
+		}
+	}
+
+	private List<Comment> getComments(TestCase tc, ASTNode node) {
+		int start = tc.getStartLineNumber(node);
+		int end = tc.getEndLineNumber(node);
+		List<Comment> comments = new ArrayList<>();
+		for (Object object : tc.getCompilationUnit().getCommentList()) {
+			Comment comment = (Comment) object;
+			if (start <= tc.getStartLineNumber(comment) && tc.getEndLineNumber(comment) <= end) {
+				comments.add(comment);
+			}
+		}
+		return comments;
+	}
+
+	private String getContent(String content, Comment comment) {
+		char[] charArray = content.toCharArray();
+		char[] ret = new char[comment.getLength()];
+		for (int offset = 0; offset < comment.getLength(); offset++) {
+			ret[offset] = charArray[comment.getStartPosition() + offset];
+		}
+		return String.valueOf(ret);
 	}
 
 	@Override
