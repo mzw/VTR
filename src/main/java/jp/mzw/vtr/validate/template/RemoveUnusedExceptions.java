ecc40cc4719fce09ccb043e903d683e8e72b69e6
Yuta Maezawa <maezawa@nii.ac.jp>
2017-01-20 14:28:22 +0900

    implement RemoveUnusedExceptions in draft (#61)

---
+++ src/main/java/jp/mzw/vtr/validate/template/RemoveUnusedExceptions.java
@@ -0,0 +1,88 @@
+package jp.mzw.vtr.validate.template;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.mzw.vtr.core.Project;
+import jp.mzw.vtr.maven.TestCase;
+import jp.mzw.vtr.validate.SimpleValidatorBase;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+import org.eclipse.text.edits.MalformedTreeException;
+import org.eclipse.text.edits.TextEdit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RemoveUnusedExceptions extends SimpleValidatorBase {
+	protected static Logger LOGGER = LoggerFactory.getLogger(RemoveUnusedExceptions.class);
+
+	public RemoveUnusedExceptions(Project project) {
+		super(project);
+	}
+
+	@Override
+	protected List<ASTNode> detect(TestCase tc) throws IOException, MalformedTreeException, BadLocationException {
+		final List<ASTNode> ret = new ArrayList<>();
+		// Collect throwable exceptions
+		final List<ITypeBinding> exceptions = new ArrayList<>();
+		tc.getMethodDeclaration().accept(new ASTVisitor() {
+			@Override
+			public boolean visit(MethodInvocation node) {
+				IMethodBinding binding = node.resolveMethodBinding();
+				if (binding == null) {
+					return super.visit(node);
+				}
+				for (ITypeBinding exception : binding.getExceptionTypes()) {
+					if (!exceptions.contains(exception)) {
+						exceptions.add(exception);
+					}
+				}
+				return super.visit(node);
+			}
+		});
+		// Determine whether thrown exceptions are used
+		for (Object object : tc.getMethodDeclaration().thrownExceptionTypes()) {
+			SimpleType thrown = (SimpleType) object;
+			ITypeBinding type = thrown.resolveBinding();
+			boolean used = false;
+			for (ITypeBinding exception : exceptions) {
+				if (exception.equals(type)) {
+					used = true;
+					break;
+				}
+			}
+			if (!used) {
+				ret.add(thrown);
+			}
+		}
+		return ret;
+	}
+
+	@Override
+	protected String getModified(String origin, TestCase tc) throws IOException, MalformedTreeException, BadLocationException {
+		List<ASTNode> detects = detect(tc);
+		if (detects.isEmpty()) {
+			return origin;
+		}
+		AST ast = detects.get(0).getAST();
+		ASTRewrite rewrite = ASTRewrite.create(ast);
+		for (ASTNode detect : detects) {
+			rewrite.remove(detect, null);
+		}
+		// modify
+		Document document = new Document(origin);
+		TextEdit edit = rewrite.rewriteAST(document, null);
+		edit.apply(document);
+		return document.get();
+	}
+}
