47ec3136236558d231216b9b5dedf359dccaeb68
Yuta Maezawa <maezawa@nii.ac.jp>
2017-02-02 19:09:27 +0900

    modify AddOverrideAnnotation accorfing to the API change (24770904)

--- src/main/java/jp/mzw/vtr/validate/template/AddOverrideAnnotation.java
+++ src/main/java/jp/mzw/vtr/validate/template/AddOverrideAnnotation.java
@@ -1,6 +1,8 @@
 package jp.mzw.vtr.validate.template;
 
 import jp.mzw.vtr.core.Project;
+import jp.mzw.vtr.git.Commit;
+import jp.mzw.vtr.maven.Results;
 import jp.mzw.vtr.maven.TestCase;
 import jp.mzw.vtr.validate.SimpleValidatorBase;
 import org.eclipse.jdt.core.dom.*;
@@ -20,55 +22,58 @@ import java.util.List;
  * Created by TK on 2017/02/02.
  */
 public class AddOverrideAnnotation extends SimpleValidatorBase {
-    protected static Logger LOGGER = LoggerFactory.getLogger(AddOverrideAnnotation.class);
+	protected static Logger LOGGER = LoggerFactory.getLogger(AddOverrideAnnotation.class);
 
-    public AddOverrideAnnotation(Project project) {
-        super(project);
-    }
+	public AddOverrideAnnotation(Project project) {
+		super(project);
+	}
 
-    @Override
-    protected List<ASTNode> detect(TestCase tc) throws IOException, MalformedTreeException, BadLocationException {
-        final List<ASTNode> ret = new ArrayList<>();
-        IMethodBinding thisMethodBinding = tc.getMethodDeclaration().resolveBinding();
-        ITypeBinding typeBinding = thisMethodBinding.getDeclaringClass();
-        while (typeBinding != null) {
-            boolean overrides = false;
-            for (IMethodBinding method: typeBinding.getDeclaredMethods()) {
-                if (thisMethodBinding.overrides(method)) {
-                    overrides = true;
-                    ret.add(tc.getMethodDeclaration());
-                    break;
-                }
-            }
-            if (overrides) {
-                break;
-            }
-            typeBinding = typeBinding.getSuperclass();
-        }
-        return ret;
-    }
+	@Override
+	protected List<ASTNode> detect(final Commit commit, final TestCase tc, final Results results)
+			throws IOException, MalformedTreeException, BadLocationException {
+		final List<ASTNode> ret = new ArrayList<>();
+		IMethodBinding thisMethodBinding = tc.getMethodDeclaration().resolveBinding();
+		ITypeBinding typeBinding = thisMethodBinding.getDeclaringClass();
+		while (typeBinding != null) {
+			boolean overrides = false;
+			for (IMethodBinding method : typeBinding.getDeclaredMethods()) {
+				if (thisMethodBinding.overrides(method)) {
+					overrides = true;
+					ret.add(tc.getMethodDeclaration());
+					break;
+				}
+			}
+			if (overrides) {
+				break;
+			}
+			typeBinding = typeBinding.getSuperclass();
+		}
+		return ret;
+	}
 
-    @Override
-    protected String getModified(String origin, TestCase tc) throws IOException, MalformedTreeException, BadLocationException {
-        // prepare
-        CompilationUnit cu = tc.getCompilationUnit();
-        AST ast = cu.getAST();
-        ASTRewrite rewrite = ASTRewrite.create(ast);
-        // detect
-        for (ASTNode node: detect(tc)) {
-            MethodDeclaration target = (MethodDeclaration) node;
-            MethodDeclaration replace = (MethodDeclaration) ASTNode.copySubtree(ast, node);
-            // create new annotation
-            MarkerAnnotation annotation = ast.newMarkerAnnotation();
-            annotation.setTypeName(ast.newName("Override"));
-            // insert annotation
-            replace.modifiers().add(0, annotation);
-            rewrite.replace(target, replace, null);
-        }
-        // modify
-        Document document = new Document(origin);
-        TextEdit edit = rewrite.rewriteAST(document, null);
-        edit.apply(document);
-        return document.get();
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	protected String getModified(final String origin, final Commit commit, final TestCase tc, final Results results)
+			throws IOException, MalformedTreeException, BadLocationException {
+		// prepare
+		CompilationUnit cu = tc.getCompilationUnit();
+		AST ast = cu.getAST();
+		ASTRewrite rewrite = ASTRewrite.create(ast);
+		// detect
+		for (ASTNode node : detect(commit, tc, results)) {
+			MethodDeclaration target = (MethodDeclaration) node;
+			MethodDeclaration replace = (MethodDeclaration) ASTNode.copySubtree(ast, node);
+			// create new annotation
+			MarkerAnnotation annotation = ast.newMarkerAnnotation();
+			annotation.setTypeName(ast.newName("Override"));
+			// insert annotation
+			replace.modifiers().add(0, annotation);
+			rewrite.replace(target, replace, null);
+		}
+		// modify
+		Document document = new Document(origin);
+		TextEdit edit = rewrite.rewriteAST(document, null);
+		edit.apply(document);
+		return document.get();
+	}
 }
