49e5323bb9b8e5636dd6f73b26450092a9adda96
Yuta Maezawa <maezawa@nii.ac.jp>
2017-02-13 19:25:12 +0900

    #162

--- src/main/java/jp/mzw/vtr/validate/junit/AddTestAnnotations.java
+++ src/main/java/jp/mzw/vtr/validate/junit/AddTestAnnotations.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
@@ -21,10 +23,10 @@ import org.slf4j.LoggerFactory;
 
 import jp.mzw.vtr.core.Project;
 import jp.mzw.vtr.git.Commit;
-import jp.mzw.vtr.maven.MavenUtils;
 import jp.mzw.vtr.maven.Results;
 import jp.mzw.vtr.maven.TestCase;
 import jp.mzw.vtr.validate.SimpleValidatorBase;
+import jp.mzw.vtr.validate.ValidatorBase;
 import jp.mzw.vtr.validate.ValidatorUtils;
 
 public class AddTestAnnotations extends SimpleValidatorBase {
@@ -37,6 +39,9 @@ public class AddTestAnnotations extends SimpleValidatorBase {
 	@Override
 	protected List<ASTNode> detect(final Commit commit, final TestCase tc, final Results results) throws IOException, MalformedTreeException, BadLocationException {
 		final List<ASTNode> ret = new ArrayList<>();
+		if (Version.parse("4").compareTo(ValidatorBase.getJunitVersion(projectDir)) < 0) {
+			return ret;
+		}
 		if (!ValidatorUtils.hasTestAnnotation(tc)) {
 			ret.add(tc.getMethodDeclaration());
 		}
@@ -45,79 +50,56 @@ public class AddTestAnnotations extends SimpleValidatorBase {
 
 	@Override
 	protected String getModified(String origin, final Commit commit, final TestCase tc, final Results results) throws IOException, MalformedTreeException, BadLocationException {
-		String hasTestAnnot = insertTestAnnotation(origin, tc);
-		String hasJunitImports = insertJuitImports(hasTestAnnot, tc);
-		return hasJunitImports;
-	}
-
-	/**
-	 * Insert test annotation
-	 * 
-	 * @param origin
-	 * @param tc
-	 * @return
-	 * @throws MalformedTreeException
-	 * @throws BadLocationException
-	 */
-	private String insertTestAnnotation(String origin, TestCase tc) throws MalformedTreeException, BadLocationException {
-		ASTRewrite rewriter = ASTRewrite.create(tc.getCompilationUnit().getAST());
-		MethodDeclaration method = tc.getMethodDeclaration();
-		ListRewrite lr = rewriter.getListRewrite(method, method.getModifiersProperty());
-		lr.insertFirst(rewriter.createStringPlaceholder("@Test", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
-		Document document = new Document(origin);
-		TextEdit edit = rewriter.rewriteAST(document, null);
-		edit.apply(document);
-		return document.get();
-	}
-
-	/**
-	 * Insert "org.junit.Test" import and remove "junit.framework" imports, if
-	 * any
-	 * 
-	 * @param origin
-	 * @param tc
-	 * @return
-	 * @throws MalformedTreeException
-	 * @throws BadLocationException
-	 */
-	private String insertJuitImports(String origin, TestCase tc) throws MalformedTreeException, BadLocationException {
-		List<ImportDeclaration> removeImports = new ArrayList<>();
-		List<Type> removeTypes = new ArrayList<>();
-		boolean isJunitTestImport = false;
-		ASTNode root = tc.getMethodDeclaration().getRoot();
-		for (ASTNode node : MavenUtils.getChildren(root)) {
-			if (node instanceof ImportDeclaration) {
-				ImportDeclaration importDec = (ImportDeclaration) node;
-				if (importDec.getName().toString().startsWith("junit.framework")) {
-					removeImports.add(importDec);
-				} else if (importDec.getName().toString().equals("import org.junit.Test")) {
-					isJunitTestImport = true;
-				} else if (importDec.getName().toString().equals("import org.junit.*")) {
-					isJunitTestImport = true;
-				}
-			} else if (node instanceof TypeDeclaration) {
-				TypeDeclaration typeDec = (TypeDeclaration) node;
-				Type type = typeDec.getSuperclassType();
-				if (type != null) {
-					if ("TestCase".equals(type.toString())) {
-						removeTypes.add(type);
-					}
+		// prepare
+		final CompilationUnit cu = tc.getCompilationUnit();
+		final AST ast = cu.getAST();
+		final ASTRewrite rewrite = ASTRewrite.create(ast);
+		// add Test annotation
+		{
+			MethodDeclaration method = tc.getMethodDeclaration();
+			ListRewrite lr = rewrite.getListRewrite(method, method.getModifiersProperty());
+			lr.insertFirst(rewrite.createStringPlaceholder("@Test", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
+			lr.insertFirst(rewrite.createStringPlaceholder("@Test", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
+		}
+		// import Test if nothing
+		{
+			boolean imported = false;
+			List<ImportDeclaration> removes = new ArrayList<>();
+			for (Object object : cu.imports()) {
+				ImportDeclaration current = (ImportDeclaration) object;
+				if (current.getName().toString().startsWith("junit.framework")) {
+					removes.add(current);
+				} else if (current.getName().toString().equals("import org.junit.Test")) {
+					imported = true;
+				} else if (current.getName().toString().equals("import org.junit.*")) {
+					imported = true;
 				}
 			}
+			if (!imported) {
+				ListRewrite lr = rewrite.getListRewrite(tc.getCompilationUnit(), CompilationUnit.IMPORTS_PROPERTY);
+				lr.insertLast(rewrite.createStringPlaceholder("import org.junit.Test;", ASTNode.IMPORT_DECLARATION), null);
+			}
+			ASTRewrite rewriter = ASTRewrite.create(tc.getCompilationUnit().getAST());
+			for (ImportDeclaration remove : removes) {
+				rewriter.remove(remove, null);
+			}
 		}
-		ASTRewrite rewriter = ASTRewrite.create(tc.getCompilationUnit().getAST());
-		for (ImportDeclaration remove : removeImports) {
-			rewriter.remove(remove, null);
-		}
-		if (!isJunitTestImport) {
-			ListRewrite lr = rewriter.getListRewrite(tc.getCompilationUnit(), CompilationUnit.IMPORTS_PROPERTY);
-			lr.insertLast(rewriter.createStringPlaceholder("import org.junit.Test;", ASTNode.IMPORT_DECLARATION), null);
-		}
-		for (Type remove : removeTypes) {
-			rewriter.remove(remove, null);
-		}
+		// remove types
+		cu.accept(new ASTVisitor() {
+			@Override
+			public boolean visit(TypeDeclaration node) {
+				Type type = node.getSuperclassType();
+				if (type != null) {
+					if ("TestCase".equals(type.toString())) {
+						rewrite.remove(node, null);
+					}
+				}
+				return super.visit(node);
+			}
+		});
+		// modify
 		Document document = new Document(origin);
-		TextEdit edit = rewriter.rewriteAST(document, null);
+		TextEdit edit = rewrite.rewriteAST(document, null);
 		edit.apply(document);
 		return document.get();
 	}
