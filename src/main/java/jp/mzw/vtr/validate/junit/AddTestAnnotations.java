fa71826eba328e029837121b0d131be12a935681
Yuta Maezawa <maezawa@nii.ac.jp>
2017-02-18 17:01:14 +0900

    #197

--- src/main/java/jp/mzw/vtr/validate/junit/AddTestAnnotations.java
+++ src/main/java/jp/mzw/vtr/validate/junit/AddTestAnnotations.java
@@ -10,6 +10,7 @@ import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SuperMethodInvocation;
 import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
@@ -37,7 +38,8 @@ public class AddTestAnnotations extends SimpleValidatorBase {
 	}
 
 	@Override
-	protected List<ASTNode> detect(final Commit commit, final TestCase tc, final Results results) throws IOException, MalformedTreeException, BadLocationException {
+	protected List<ASTNode> detect(final Commit commit, final TestCase tc, final Results results)
+			throws IOException, MalformedTreeException, BadLocationException {
 		final List<ASTNode> ret = new ArrayList<>();
 		if (Version.parse("4").compareTo(ValidatorBase.getJunitVersion(projectDir)) < 0) {
 			return ret;
@@ -49,7 +51,8 @@ public class AddTestAnnotations extends SimpleValidatorBase {
 	}
 
 	@Override
-	protected String getModified(String origin, final Commit commit, final TestCase tc, final Results results) throws IOException, MalformedTreeException, BadLocationException {
+	protected String getModified(String origin, final Commit commit, final TestCase tc, final Results results)
+			throws IOException, MalformedTreeException, BadLocationException {
 		// prepare
 		final CompilationUnit cu = tc.getCompilationUnit();
 		final AST ast = cu.getAST();
@@ -57,9 +60,8 @@ public class AddTestAnnotations extends SimpleValidatorBase {
 		// add Test annotation
 		{
 			MethodDeclaration method = tc.getMethodDeclaration();
-			ListRewrite lr = rewrite.getListRewrite(method, method.getModifiersProperty());
-			lr.insertFirst(rewrite.createStringPlaceholder("@Test", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
-			lr.insertFirst(rewrite.createStringPlaceholder("@Test", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
+			ListRewrite modifiers = rewrite.getListRewrite(method, method.getModifiersProperty());
+			modifiers.insertFirst(rewrite.createStringPlaceholder("@Test", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
 		}
 		// import Test if nothing
 		{
@@ -76,31 +78,89 @@ public class AddTestAnnotations extends SimpleValidatorBase {
 				}
 			}
 			if (!imported) {
-				ListRewrite lr = rewrite.getListRewrite(tc.getCompilationUnit(), CompilationUnit.IMPORTS_PROPERTY);
-				lr.insertLast(rewrite.createStringPlaceholder("import org.junit.Test;", ASTNode.IMPORT_DECLARATION), null);
+				ListRewrite imports = rewrite.getListRewrite(cu, CompilationUnit.IMPORTS_PROPERTY);
+				imports.insertLast(rewrite.createStringPlaceholder("import org.junit.Test;", ASTNode.IMPORT_DECLARATION), null);
 			}
-			ASTRewrite rewriter = ASTRewrite.create(tc.getCompilationUnit().getAST());
 			for (ImportDeclaration remove : removes) {
-				rewriter.remove(remove, null);
+				rewrite.remove(remove, null);
 			}
 		}
 		// remove types
-		cu.accept(new ASTVisitor() {
-			@Override
-			public boolean visit(TypeDeclaration node) {
-				Type type = node.getSuperclassType();
-				if (type != null) {
-					if ("TestCase".equals(type.toString())) {
-						rewrite.remove(node, null);
+		{
+			final List<MethodDeclaration> setups = new ArrayList<>();
+			final List<MethodDeclaration> teardowns = new ArrayList<>();
+			cu.accept(new ASTVisitor() {
+				@Override
+				public boolean visit(TypeDeclaration node) {
+					Type type = node.getSuperclassType();
+					if (type != null) {
+						if ("TestCase".equals(type.toString())) {
+							rewrite.remove(type, null);
+						}
 					}
+					return super.visit(node);
 				}
-				return super.visit(node);
+
+				@Override
+				public boolean visit(SuperMethodInvocation node) {
+					String name = node.getName().toString();
+					MethodDeclaration method = getParentMethodDeclaration(node);
+					if (method == null) {
+						return super.visit(node);
+					}
+					if ("setUp".equals(name)) {
+						if (!setups.contains(method)) {
+							setups.add(method);
+						}
+						rewrite.remove(node.getParent(), null);
+					} else if ("tearDown".equals(name)) {
+						if (!teardowns.contains(method)) {
+							teardowns.add(method);
+						}
+						rewrite.remove(node.getParent(), null);
+					}
+					return super.visit(node);
+				}
+			});
+			if (!setups.isEmpty()) {
+				ListRewrite imports = rewrite.getListRewrite(cu, CompilationUnit.IMPORTS_PROPERTY);
+				imports.insertLast(rewrite.createStringPlaceholder("import org.junit.Before;", ASTNode.IMPORT_DECLARATION), null);
+				for (MethodDeclaration method : setups) {
+					ListRewrite modifiers = rewrite.getListRewrite(method, method.getModifiersProperty());
+					modifiers.insertFirst(rewrite.createStringPlaceholder("@Before", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
+				}
 			}
-		});
+			if (!teardowns.isEmpty()) {
+				ListRewrite imports = rewrite.getListRewrite(cu, CompilationUnit.IMPORTS_PROPERTY);
+				imports.insertLast(rewrite.createStringPlaceholder("import org.junit.After;", ASTNode.IMPORT_DECLARATION), null);
+				for (MethodDeclaration method : teardowns) {
+					ListRewrite modifiers = rewrite.getListRewrite(method, method.getModifiersProperty());
+					modifiers.insertFirst(rewrite.createStringPlaceholder("@After", ASTNode.ANNOTATION_TYPE_DECLARATION), null);
+				}
+			}
+		}
+
 		// modify
 		Document document = new Document(origin);
 		TextEdit edit = rewrite.rewriteAST(document, null);
 		edit.apply(document);
 		return document.get();
+	}
+
+	public static MethodDeclaration getParentMethodDeclaration(ASTNode node) {
+		if (node == null) {
+			return null;
+		}
+		if (node instanceof MethodDeclaration) {
+			return (MethodDeclaration) node;
+		}
+		ASTNode parent = node.getParent();
+		while (parent != null) {
+			if (parent instanceof MethodDeclaration) {
+				return (MethodDeclaration) parent;
+			}
+			parent = parent.getParent();
+		}
+		return null;
 	}
 }
