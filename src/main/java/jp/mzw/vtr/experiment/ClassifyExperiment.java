7d0926ca783b28a0b5f5d756aa431bc480941be9
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2017-10-30 15:10:30 +0900

    print stats for NullAST and NullCommit

--- src/main/java/jp/mzw/vtr/experiment/ClassifyExperiment.java
+++ src/main/java/jp/mzw/vtr/experiment/ClassifyExperiment.java
@@ -30,7 +30,7 @@ public class ClassifyExperiment {
             "#41", "#42",
             "#L1", "#L2", "#L3", "#L4", "#L5", "#L6", "#L7", "#L8", "#L9",
             "#FP1",
-            "Nan"
+            "Nan", "NullAST", "NullCommit"
     };
 
     static final String TRUE_POSITIVE  = ": true positive";
