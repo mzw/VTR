517189ae9e2956cb4b42a3c59a50824030e5fd3b
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2017-11-07 17:10:39 +0900

    use Ast nodes for clasifying AddTestAnnotation(#1)

--- src/main/java/jp/mzw/vtr/classify/pattern/AddTestAnnotationPattern.java
+++ src/main/java/jp/mzw/vtr/classify/pattern/AddTestAnnotationPattern.java
@@ -2,6 +2,8 @@ package jp.mzw.vtr.classify.pattern;
 
 import jp.mzw.vtr.detect.TestCaseModification;
 
+import java.util.List;
+
 public class AddTestAnnotationPattern {
     static public boolean match(TestCaseModification testCaseModification) {
         for (String keyword : KEYWORDS) {
@@ -9,10 +11,30 @@ public class AddTestAnnotationPattern {
                 return true;
             }
         }
+
+        if (testCaseModification.getOriginalNodeClassesWithText().isEmpty()
+                && (0 < countNumOfTestAnnotation(testCaseModification.getRevisedNodeClassesWithText()))
+                && onlyTestAnnotation(testCaseModification.getRevisedNodeClassesWithText())) {
+            return true;
+        }
         return false;
     }
 
-    private static String[] KEYWORDS = {
+    static private String[] KEYWORDS = {
             "junit 4 annotation style"
     };
+
+    static private long countNumOfTestAnnotation(List<String> nodes) {
+        return nodes.stream().filter(node -> node.startsWith("org.eclipse.jdt.core.dom.MarkerAnnotation:@Test")).count();
+    }
+
+    static private boolean onlyTestAnnotation(List<String> nodes) {
+        for (String node : nodes) {
+            if (!(node.startsWith("org.eclipse.jdt.core.dom.MarkerAnnotation:@Test")
+                    || node.startsWith("org.eclipse.jdt.core.dom.SimpleName:Test"))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
